5146e2854864f15ec8deff5bdac3f8dff7d69953
yashraj-shitole <yashrajs927@gmail.com>
2024-05-09 18:28:31 +0530

    cursor

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
+import AnimatedCursor from "react-animated-cursor"
 
 
 const Layout = () => {
@@ -9,6 +10,27 @@ const Layout = () => {
   return (
 
  <div className=' overflow-hidden'>
+  <AnimatedCursor
+      innerSize={20}
+      outerSize={15}
+      color='10, 10, 10'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link'
+      ]}
+    />
                 <Navbar/>
 
 
@@ -17,6 +39,7 @@ const Layout = () => {
         </div>
 
           <Footer/>
+
     </div>
 
 
